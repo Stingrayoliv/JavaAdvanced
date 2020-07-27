830aeb60d4ca4c6d605317ac5c281111dd8913e6
stingrey <maria.morenko06@gmail.com>
2020-07-27 21:13:18 +0200

    Lesson 09 Card

---
+++ src/lesson09/Card.java
@@ -0,0 +1,36 @@
+package lesson09;
+
+/**
+ * JavaAdvanced
+ * 27.07.20 19: 23
+ */
+public abstract class Card {
+    private Person person;
+    private String number;
+
+    public Card(Person person, String number) {
+        this.person = person;
+        this.number = number;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "person=" + person +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    public abstract String toSecureString();
+
+}
+
+
---
+++ src/lesson09/Master.java
@@ -0,0 +1,35 @@
+package lesson09;
+
+/**
+ * JavaAdvanced
+ * 27.07.20 20: 10
+ */
+public class Master extends Card {
+    public Master(Person person, String number) {
+        super(person, number);
+    }
+
+    @Override
+    public String toString() {
+        return "Master{" +
+                "person=" + getPerson() +
+                ", number='" + getNumber() + '\'' +
+                '}';
+    }
+
+    @Override
+    public String toSecureString(){
+        return "Master{" +
+                "person=" + getPerson() +
+                ", number='" + getSecureNumber() + '\'' +
+                '}';
+    }
+
+    private String getSecureNumber() {
+        String star = "";
+        for (int i = 0; i < getNumber().length() - 4; i++) {
+            star += "*";
+        }
+        return getNumber().substring(0, 2) + star + getNumber().substring(getNumber().length() - 2);
+    }
+}
