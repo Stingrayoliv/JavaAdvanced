b916c7114c3814c4c6c88cfaf035b7d16eef922a
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-11-29 20:41:23 +0100

    Task "Find a number without a pair". The second loop was added.

--- src/moduleSecond/tasks/TaskOne.java
+++ src/moduleSecond/tasks/TaskOne.java
@@ -1,26 +1,26 @@
 package moduleSecond.tasks;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class TaskOne {
 
     //Есть лист целых (Integer) чисел. Известно что каждое число, кроме одного, имеет пару. Найти число без пары.
-// {3, 5, 3}->5; {3,5,3,3,3}->5; {3,5,3,5,3}->3
+    //{3, 5, 3}->5; {3,5,3,3,3}->5; {3,5,3,5,3}->3
     public static int findNumberWithoutPair(List<Integer> list) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
         if (!list.isEmpty()) {
-            Map<Integer, Integer> map = new HashMap<>();
             int count;
             for (Integer integer : list) {
                 count = Collections.frequency(list, integer);
-                if (count % 2 == 1) {
-                    map.put(1, integer);// only 1 number can be without a pair
-                }
+                map.put(integer, count);
             }
-            return map.get(1);
-        } else {
-            return 1;
         }
+        for (Map.Entry<Integer, Integer> item : map.entrySet()) {
+            if (item.getValue() % 2 != 0) {
+                result = item.getKey();
+            }
+        }
+        return result;
     }
 }
